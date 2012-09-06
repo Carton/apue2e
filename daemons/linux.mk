70ecb8706f1919d24476c34ec69d5b070fab03e5
Carton He <cartonhe@gmail.com>
2012-09-06 23:39:01 +0800

    Make makefile work

--- daemons/linux.mk
+++ daemons/linux.mk
@@ -1,17 +1,18 @@
 include ../Make.defines.linux
 EXTRA=
+EXTRALIBS=-pthread
 
-all:	init.o lockfile.o reread.o reread2.o single.o
+all:	init.o lockfile.o reread reread2 single.o
 
 init.o:		init.c
 
 lockfile.o:	lockfile.c
 
-reread.o:	reread.c
-
-reread2.o:	reread2.c
-
 single.o:	single.c
+
+reread: reread.o single.o lockfile.o init.o
+
+reread2: reread2.o single.o lockfile.o init.o
 
 clean:
 	rm -f ${PROGS} ${TEMPFILES} *.o
--- signals/linux.mk
+++ signals/linux.mk
@@ -1,7 +1,7 @@
 include ../Make.defines.linux
 
 PROGS =	child critical mask read1 read2 reenter sigtstp \
-	sigusr suspend1 suspend2 tsleep2
+	sigusr suspend1 suspend2 tsleep2 alarm
 
 all:	abort.o sleep1.o sleep2.o system.o tsleep2.o ${PROGS}
 
